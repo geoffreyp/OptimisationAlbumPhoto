8574e3addcac976e01fbb730e6c9dab49e5a46e4
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-12-10 17:17:57 +0100

    Add a new Album class
    The evaluation function evalue the average hash distance between photos

---
+++ src/album/AlbumAverageHash.java
@@ -0,0 +1,28 @@
+package album;
+
+import algorithm.LocalSearchAlgorithm;
+import tools.Album;
+import tools.HillClimber;
+
+public class AlbumAverageHash extends LocalSearchAlgorithm {
+
+	public AlbumAverageHash(int taille, boolean debug) {
+		super(debug);
+		this.nb_photos = taille;
+		solution = HillClimber.generateShuffleSolution(taille);
+	}
+
+	/**
+	 * @author Geoffrey Pruvost
+	 */
+	@Override
+	public double eval(int[] solution) {
+		double sum = 0;
+		for (int i = 0; i < solution.length-1; i++) {
+			sum += Album.getAverageHashDistance(solution[i], solution[i+1]);
+		}
+		return sum;
+
+	}
+
+}
