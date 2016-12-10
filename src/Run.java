6260242da337703285402d47558460b39a1c792b
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-12-10 19:15:00 +0100

    update Run.java to use args

--- src/Run.java
+++ src/Run.java
@@ -1,25 +1,49 @@
-import album.AlbumPhotoRandom;
+import album.AlbumAverageHash;
+import album.AlbumRandom;
+import tools.Album;
 
 public class Run {
 
 	public static void main(String[] args) {
-		//
-		// double best = 1000;
-		//
-		// for (int i = 0; i < 1000; i++) {
-		// AlbumPhoto a = new AlbumPhoto(55, true);
-		// double t = a.hillClimberFirstImprovement(10000);
-		//
-		// if (t < best) {
-		// best = t;
-		// }
-		// }
-		// System.out.println(best);
-
-		AlbumPhotoRandom b = new AlbumPhotoRandom(55, true);
-		int nb_parents = 55;
-		int nb_geniteur = 20;
-		int nb_generations = 50;
-		b.algoEvolutionnaire(nb_parents, nb_geniteur, nb_generations);
+		switch (args[0]) {
+			case "rand":
+				System.out.println("Random Album");
+				double bestrand = 1000;
+				for (int i = 0; i < new Integer(args[1]); i++) {
+					AlbumRandom a = new AlbumRandom(55, false);
+					int[] sh = a.hillClimberFirstImprovement(10000);
+					
+					if(bestrand > a.eval(sh)){
+						bestrand = a.eval(sh);
+						Album.writeSolution(sh);
+						System.out.println("ecrit "+bestrand);
+					}
+				}
+				
+				System.out.println("Evaluation : "+bestrand);
+				break;
+				
+			case "ahash":
+				System.out.println("Average Album");
+				double best = 1000;
+				for (int i = 0; i < new Integer(args[1]); i++) {
+					AlbumAverageHash ah = new AlbumAverageHash(55, false);
+					int[] sh = ah.hillClimberFirstImprovement(10000);
+					
+					if(best > ah.eval(sh)){
+						best = ah.eval(sh);
+						Album.writeSolution(sh);
+						System.out.println("ecrit "+best);
+					}
+				}
+				
+				System.out.println("Evaluation : "+best);
+				
+				break;
+			
+			default:
+				System.err.println("Error");
+				break;
+		}
 	}
 }
